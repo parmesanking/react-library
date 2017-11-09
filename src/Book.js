24ab896f20e6ff9f8d622d754a09c6605e6e1614
Luca Agostini <luca.agostini@yourvoice.com>
2017-11-09 07:29:21 +0100

    fix: Handled missing thumbnail

--- src/Book.js
+++ src/Book.js
@@ -5,6 +5,7 @@ import ShelfSelector from './ShelfSelector.js'
 class Book extends React.Component {
   
   render(){
+    //on certain books imageLinks or thumbnail are not set
     let thumbNail = (this.props.book.imageLinks && this.props.book.imageLinks.thumbnail) ?  
                         this.props.book.imageLinks.thumbnail ? 
                           this.props.book.imageLinks.thumbnail 
@@ -15,9 +16,10 @@ class Book extends React.Component {
       <div>
         <div className="book">
           <div className="book-top">
-
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + thumbNail + '")' }}></div>
-            {this.props.selector && <ShelfSelector book={this.props.book} onShelfChange={this.props.onShelfChange}/>}
+            <a href="#" onClick={this.props.onBookPick(this.props.book.id)} >
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + thumbNail + '")' }}></div>
+              {this.props.selector && <ShelfSelector book={this.props.book} onShelfChange={this.props.onShelfChange}/>}
+            </a>
           </div>
           <div className="book-title">{this.props.book.title}</div>
           <div className="book-authors">{this.props.book.authors && this.props.book.authors.map((author) => {
